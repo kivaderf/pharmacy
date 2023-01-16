1baca186a5190ce8f190db21c4c49ac08568a5f7
kivaderf <filip243@gmail.com>
2023-01-16 01:59:57 +0100

    added deadline counter

--- resources/views/frontend/layout/footer.blade.php
+++ resources/views/frontend/layout/footer.blade.php
@@ -1,4 +1,7 @@
     <footer class="fixed-bottom">
+        <div id="countdown">
+            <iframe src="https://free.timeanddate.com/countdown/i8ogwqbd/n3991/cf11/cm0/cu4/ct0/cs0/ca0/co0/cr0/ss0/cac000/cpc000/pcfff/tcfff/fs100/szw320/szh135/iso2023-01-17T23:55:00" allowtransparency="true" width="290" height="19" frameborder="0"></iframe>
+        </div>
         <div id="copyright" class="container">
             <div class="row">
                 <div class="col-5"><p>FRI Lekáreň</p></div>
